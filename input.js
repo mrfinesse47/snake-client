60bab170ed028c8598558a1f525e9af5eeb452d3
Kevin Mason <krmason2020@gmail.com>
2021-11-03 22:02:59 +0000

    enabled a message mode

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 let connection;
+let isMessageMode = false;
+let tmpMsg = "";
 
-const setupInput = function(conn) {
+const setupInput = function (conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,18 +13,37 @@ const setupInput = function(conn) {
   return stdin;
 };
 
-const handleUserInput = function(key) {
+const handleUserInput = function (key) {
   //console.log(key);
+  //Say: _____ sends message
+
   if (key === "\u0003") {
     process.exit();
-  } else if (key === "w") {
-    connection.write("Move: up");
-  } else if (key === "a") {
-    connection.write("Move: left");
-  } else if (key === "s") {
-    connection.write("Move: down");
-  } else if (key === "d") {
-    connection.write("Move: right");
+  }
+
+  if (!isMessageMode) {
+    if (key === "w") {
+      connection.write("Move: up");
+    } else if (key === "a") {
+      connection.write("Move: left");
+    } else if (key === "s") {
+      connection.write("Move: down");
+    } else if (key === "d") {
+      connection.write("Move: right");
+    } else if (key === "\u000d") {
+      isMessageMode = true;
+      process.stdout.write("\nSend Message:");
+    }
+  } else {
+    tmpMsg += key;
+    process.stdout.write(key);
+    if (key === "\u000d") {
+      isMessageMode = false;
+      connection.write("Say: " + tmpMsg);
+      // console.log("returns");
+      process.stdout.write("\nContinue Playing!");
+      tmpMsg = "";
+    }
   }
 };
 
