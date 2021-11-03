ef4ed27c658e1df3a155e3d0be9d6c9386035eb8
Kevin Mason <krmason2020@gmail.com>
2021-11-03 20:29:21 +0000

    set up input using stdin

--- play.js
+++ play.js
@@ -1,6 +1,27 @@
 // IP: 165.227.47.243
 // PORT: 5054
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  console.log(key);
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+const stdin = setupInput();
+
+stdin.on("data", handleUserInput);
+
 const { connect } = require("./client");
 
 // establishes a connection with the game server
